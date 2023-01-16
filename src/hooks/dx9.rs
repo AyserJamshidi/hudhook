f0ac5f3eb06cf50ec0d6eeec66a8480ee84a1620
Ayser Jamshidi <acejamshidi4th@gmail.com>
2023-01-15 23:34:17 -0500

    Fixed DX9 ejection crash due to no DX9 cleanup

--- src/hooks/dx9.rs
+++ src/hooks/dx9.rs
@@ -207,7 +207,13 @@ impl ImguiRenderer {
         self.renderer.render(draw_data).unwrap();
     }
 
-    unsafe fn cleanup(&mut self) {}
+    unsafe fn cleanup(&mut self) {
+        #[cfg(any(target_arch = "aarch64", target_arch = "x86_64"))]
+        SetWindowLongPtrA(self.renderer.get_hwnd(), GWLP_WNDPROC, self.wnd_proc as usize as isize);
+
+        #[cfg(target_arch = "x86")]
+        SetWindowLongA(self.renderer.get_hwnd(), GWLP_WNDPROC, self.wnd_proc as usize as i32);
+	}
 }
 
 impl ImguiWindowsEventHandler for ImguiRenderer {
